fd62305efd3155b21d100e2a6f9ab895f7e58b71
Aliseviandrew <dopysk@yandex.ru>
2025-05-18 20:59:53 +0300

    After adding fixtures for goods

--- goods/admin.py
+++ goods/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+from goods.models import Categories, Products
+
+# admin.site.register(Categories) - вместо этого запишем после '*'
+# admin.site.register(Products)
+# '*'
+
+
+@admin.register(Categories)
+class CategoriesAdmin(admin.ModelAdmin):
+    # передадим, какие поля будут запоняться автоматически
+    prepopulated_fields = {"slug": ("name",)}
+
+
+@admin.register(Products)
+class ProductsAdmin(admin.ModelAdmin):
+    # передадим, какие поля будут запоняться автоматически
+    prepopulated_fields = {"slug": ("name",)}
--- goods/models.py
+++ goods/models.py
@@ -1,3 +1,52 @@
+from tabnanny import verbose
+from unicodedata import category
+from django.core.validators import slug_re
 from django.db import models
 
+
 # Create your models here.
+class Categories(models.Model):
+    # перечисляем поля и т-цы
+    # slug - фрагмент URL адреса, к-рый будет вести на следующую категорию
+    name = models.CharField(max_length=150, unique=True, verbose_name="Название")
+    slug = models.SlugField(
+        max_length=150, unique=True, blank=True, null=True, verbose_name="URL"
+    )
+
+    class Meta:
+        db_table = "category"
+        verbose_name = "Категорию"
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        return self.name
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name="Название")
+    slug = models.SlugField(
+        max_length=150, unique=True, blank=True, null=True, verbose_name="URL"
+    )
+    description = models.TextField(blank=True, null=True, verbose_name="Описание")
+    image = models.ImageField(
+        upload_to="goods_images", blank=True, null=True, verbose_name="Изображение"
+    )
+    # blank=True, null=True - применяется, если изображения не будет
+    price = models.DecimalField(
+        default=0.00, max_digits=7, decimal_places=2, verbose_name="Цена"
+    )
+    discount = models.DecimalField(
+        default=0.00, max_digits=4, decimal_places=2, verbose_name="Скидка в %"
+    )
+    quantity = models.PositiveIntegerField(default=0.00, verbose_name="Количество")
+    category = models.ForeignKey(
+        to=Categories, on_delete=models.CASCADE, verbose_name="Категория"
+    )
+
+    class Meta:
+        db_table = "product"
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+
+    def __str__(self):
+        return f"{self.name} Количество - {self.quantity}"
